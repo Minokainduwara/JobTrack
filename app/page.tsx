c3cf80539fd15395429f5908b157834c34b7eb09
Minokainduwara <minokainduwara@gmail.com>
2026-03-29 23:35:37 +0530

    UI: Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-white">
@@ -8,12 +9,12 @@ export default function Home() {
           <section className="container mx-auto px-4 py-32">
             <div className="mx-auto max-w-4xl text-center">
               <h1 className="text-black mb-6 text-6xl font-bold">A better way to track your job application </h1>
-              <p className="text-muted-foreground mb-10 text-xl">Capture, organize, and stay on job search</p>
+              <p className="text-muted-foreground mb-10 text-xl">Capture, organize, and stay on job search !</p>
               <div className="flex flex-col items-center gap-4">
-                <button>
-                  Start For Free
-                </button>
-                <p>Free forever. No payment Needed.</p>
+                <Button size="lg" className="h-12 px-8 text-lg font-medium">
+                  Start For Free <ArrowRight/>
+                </Button>
+                <p className="text-sm text-muted-foreground">Free forever. No payment Needed.</p>
               </div>
             </div>
           </section>
